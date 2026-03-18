e2b0950e7d4e0e87916050b19c75765f7961e694
Mahaveer Prasad Panigrahi <pmahaveerp86619@gmail.com>
2026-03-18 15:53:10 +0530

    [feat] added init project

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Mahaveer86619/BMO/internal/config"
+	"github.com/Mahaveer86619/BMO/internal/db"
+	"github.com/Mahaveer86619/BMO/internal/web"
+	"github.com/charmbracelet/log"
+)
 
 func main() {
-	fmt.Printf("BMO server warming up...")
+	log.Info("BMO server warming up...")
+
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal("failed to load config", "error", err)
+	}
+
+	ctx := context.Background()
+
+	pgPool, err := db.NewPostgresPool(ctx, cfg)
+	if err != nil {
+		log.Fatal("failed to connect to postgres", "error", err)
+	}
+	defer pgPool.Close()
+	log.Info("connected to postgres")
+
+	redisClient, err := db.NewRedisClient(ctx, cfg)
+	if err != nil {
+		log.Fatal("failed to connect to redis", "error", err)
+	}
+	defer redisClient.Close()
+	log.Info("connected to redis")
+
+	server := web.NewServer(cfg, pgPool, redisClient)
+
+	// Graceful shutdown
+	shutdownCtx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err := server.Start(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("server crashed", "error", err)
+		}
+	}()
+
+	log.Info("BMO server started successfully")
+
+	<-shutdownCtx.Done()
+	log.Info("shutting down...")
+
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Router.Shutdown(timeoutCtx); err != nil {
+		log.Fatal("forced shutdown", "error", err)
+	}
+
+	log.Info("BMO server shut down gracefully")
 }
--- server/internal/config/config.go
+++ server/internal/config/config.go
@@ -1,1 +1,64 @@
 package config
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	Port int
+
+	DBHost string
+	DBPort int
+	DBUser string
+	DBPass string
+	DBName string
+
+	RedisHost string
+	RedisPort int
+	RedisPass string
+	RedisDB   int
+}
+
+var Config *AppConfig
+
+func LoadConfig() (*AppConfig, error) {
+	_ = godotenv.Load()
+
+	Config = &AppConfig{
+		Port:   GetIntValue("APP_PORT", 4040),
+		DBHost: GetStringValue("DB_HOST", "localhost"),
+		DBPort: GetIntValue("DB_PORT", 5432),
+		DBUser: GetStringValue("DB_USER", "postgres"),
+		DBPass: GetStringValue("DB_PASS", "password"),
+		DBName: GetStringValue("DB_NAME", "bmo"),
+
+		RedisHost: GetStringValue("REDIS_HOST", "localhost"),
+		RedisPort: GetIntValue("REDIS_PORT", 6379),
+		RedisPass: GetStringValue("REDIS_PASS", ""),
+		RedisDB:   GetIntValue("REDIS_DB", 0),
+	}
+
+	return Config, nil
+}
+
+func GetStringValue(key string, defaultValue string) string {
+	if os.Getenv(key) == "" {
+		return defaultValue
+	}
+	return os.Getenv(key)
+}
+
+func GetIntValue(key string, defaultValue int) int {
+	valueStr := os.Getenv(key)
+	if valueStr == "" {
+		return defaultValue
+	}
+	value, err := strconv.Atoi(valueStr)
+	if err != nil {
+		return defaultValue
+	}
+	return value
+}
